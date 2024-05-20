39b73cdef3ad745431ebe59d41e70c18a2f0df8e
ramilka <lilya1976.03.03@gmail.com>
2024-05-20 23:52:38 +0600

    Уровень 1

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface CardProps {
+    rank: string;
+    suit: string;
+}
+
+const Card: React.FC<CardProps> = ({rank, suit}) => {
+    let showSuit = '';
+    if (suit === 'diams') {
+        showSuit = '♦️';
+    } else if (suit === 'heats') {
+        showSuit = '♥️';
+    } else if (suit === 'clubs') {
+        showSuit = '♣️';
+    } else if (suit === 'spades') {
+        showSuit = '♠️';
+    } else {
+        console.log('Нет такой масти.');
+    }
+
+    let nominal = '';
+    if (rank === '2' || rank === '3' ||
+        rank === '4' || rank === '5' ||
+        rank === '6' || rank === '7' ||
+        rank === '8' || rank === '9' ||
+        rank === '10' || rank === 'j' ||
+        rank === 'q' || rank === 'k') {
+        nominal = rank;
+    } else {
+        console.log('Нет такого номинала.')
+    }
+
+    return (
+        <div>
+            <span className={`card rank-${nominal} ${suit}`}>
+                <span className="rank">{nominal}</span>
+                <span className="suit">{showSuit}</span>
+            </span>
+        </div>
+    );
+};
+
+export default Card;
