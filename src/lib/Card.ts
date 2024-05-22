2d9963e343910fd05a155905349b31c27689b3b8
ramilka <lilya1976.03.03@gmail.com>
2024-05-22 13:30:52 +0600

    Уровень 2 (Создал классы Card и СardDeck)

---
+++ src/lib/Card.ts
@@ -0,0 +1,11 @@
+class Card {
+    suit: string;
+    rank: string;
+
+    constructor(rank, suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
+
+export default Card;
---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,40 @@
+import Card from "./Card.ts";
+
+class CardDeck {
+    cards: Card[];
+    ranks: string[];
+    suits: string[];
+
+    constructor() {
+        this.cards = [];
+        this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
+        this.suits = ['diams', 'hearts', 'clubs', 'spades'];
+
+        this.ranks.map(rank => {
+            this.suits.map(suit => {
+                const card = new Card(rank, suit);
+                this.cards.push(card);
+            });
+        });
+    };
+
+    getCard() {
+        const randomIndex: number = Math.floor(Math.random() * this.cards.length);
+        let randomCard = this.cards[randomIndex];
+        this.cards.splice(randomIndex, 1);
+        return randomCard;
+    };
+
+    getCards(howMany: number) {
+        const newCards = [];
+        for (let i = 0; i < howMany; i++) {
+            newCards.push(this.getCard());
+        }
+
+        return newCards;
+    };
+}
+
+const cards = new CardDeck();
+
+export default cards;
